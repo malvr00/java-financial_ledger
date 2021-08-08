6d1bf1eed72209678bc7f6f5029d012edc9c2bf4
malvr00 <77275513+malvr00@users.noreply.github.com>
2021-08-08 16:11:27 +0900

    Button add & Event add

--- FinancialLedger/src/financialLedger/FinancialLedger_Frame.java
+++ FinancialLedger/src/financialLedger/FinancialLedger_Frame.java
@@ -12,8 +12,9 @@ public class FinancialLedger_Frame extends JFrame{
 	private JTextField tf_cal, tf_money, tf_yeartot, tf_monthtot;
 	private JTextArea discription;
 	private JButton addBt, beforeBt, nextBt;
-	
+	private JLabel label1, label2, label3, label4, label5, label6, label7;
 	private int x = 20, y = 140;		// Rect 초기값
+	private String year, month;
 	public FinancialLedger_Frame(FinancialLedger_DAO db) {
 		super("용돈기입장");
 		this.db = db;
@@ -28,13 +29,20 @@ public class FinancialLedger_Frame extends JFrame{
 		setLayout(null);
 		setSize(680,600);
 		Container cpane = getContentPane();
+	  // 달력 set
+		//cal.calSet(2021, 7);
+		cal.calGet();
+		year = Integer.toString(cal.getYear());
+		month = Integer.toString(cal.getMonth());
 		
 	  // Label
-		JLabel label1 = new JLabel("날짜");
-		JLabel label2 = new JLabel("사용금액");
-		JLabel label3 = new JLabel("사용내역");
-		JLabel label4 = new JLabel("총 년 사용금액");
-		JLabel label5 = new JLabel("총 월 사용금액");
+		label1 = new JLabel("날짜");
+		label2 = new JLabel("사용금액");
+		label3 = new JLabel("사용내역");
+		label4 = new JLabel("총 년 사용금액");
+		label5 = new JLabel("총 월 사용금액");
+		label6 = new JLabel();
+		label7 = new JLabel();
 	  // Text Field
 		tf_cal = new JTextField();
 		tf_money = new JTextField();
@@ -43,7 +51,12 @@ public class FinancialLedger_Frame extends JFrame{
 		
 		discription = new JTextArea();
 		
-	// Text Field 사용여부
+	  // Button 
+		addBt = new JButton("추가");
+		beforeBt = new JButton("▼");
+		nextBt = new JButton("▲");
+		
+	  // Text Field 사용여부
 		tf_cal.setEnabled(false);
 		tf_yeartot.setEnabled(false);
 		tf_monthtot.setEnabled(false);
@@ -51,12 +64,14 @@ public class FinancialLedger_Frame extends JFrame{
 		tf_yeartot.setHorizontalAlignment(JTextField.RIGHT);
 		tf_monthtot.setHorizontalAlignment(JTextField.RIGHT);
 		
+		
 	  // Scroll 설정
 		discription.setLineWrap(true);
 		JScrollPane scrollPane1 = new JScrollPane(discription);
 		scrollPane1.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		scrollPane1.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED );
 		
+	    
 		label1.setBounds(380, 95, 60, 60);
 		tf_cal.setBounds(445, 110, 200 ,30);
 		
@@ -72,20 +87,34 @@ public class FinancialLedger_Frame extends JFrame{
 		label5.setBounds(350,470, 80, 60);
 		tf_monthtot.setBounds(450,485,200, 30);
 		
+		addBt.setBounds(585, 425, 60, 20);
+		label6.setText(year);
+		label6.setBounds(x,y-80,60,60);
+		label7.setText(month + "월");
+		label7.setBounds(x+40,y-80, 60, 60);
+		
+		beforeBt.setBounds(x+240, y-60,50,20);
+		nextBt.setBounds(x+295, y-60,50,20);
+		
+	  // Evnet 처리
+		beforeBt.addActionListener(new calMoveEvnet());
+		nextBt.addActionListener(new calMoveEvnet());
+		
 		cpane.add(label1);
 		cpane.add(label2);
 		cpane.add(label3);
 		cpane.add(label4);
 		cpane.add(label5);
+		cpane.add(label6);
+		cpane.add(label7);
 		cpane.add(tf_cal);
 		cpane.add(tf_money);
 		cpane.add(tf_yeartot);
 		cpane.add(tf_monthtot);
 		cpane.add(scrollPane1);
-		
-		//cal.calSet(2021, 5);
-		cal.calGet();                   // 달력 Set
-		
+		cpane.add(addBt);
+		cpane.add(beforeBt);
+		cpane.add(nextBt);
 		repaint();
 	}// initForm end
 	
@@ -105,4 +134,30 @@ public class FinancialLedger_Frame extends JFrame{
 		drawRects(g);
 		cal.drawCalendar(x,y,g);		// 달력 Draw
 	}// paint end
+	
+	class calMoveEvnet implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			int nyear = Integer.parseInt(year);
+			int nmonth = Integer.parseInt(month);
+			if(e.getActionCommand() == "▼") {
+				nmonth--;
+				cal.calSet(nyear, nmonth);
+				
+				year = Integer.toString(nyear);
+				month = Integer.toString(nmonth);
+				label6.setText(year);
+				label7.setText(month + "월");
+			}else {
+				nmonth++;
+				cal.calSet(nyear, nmonth);
+				
+				year = Integer.toString(nyear);
+				month = Integer.toString(nmonth);
+				label6.setText(year);
+				label7.setText(month + "월");
+			}
+			cal.calGet();
+			repaint();
+		}	
+	}
 }
